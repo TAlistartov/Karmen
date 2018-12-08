e2885d5fdb56fadaec38a07acc8e468af3e6482d
Pavlograd2\Администратор <talistratov@gmail.com>
2018-12-08 16:08:45 +0200

    Приложение запускается, изменен путь App_Start-->RouteConfig строка defauts вместо controller="Home" установлено controller = "Administrator"

--- Karmen/Karmen/App_Start/RouteConfig.cs
+++ Karmen/Karmen/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Karmen
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Administrator", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
